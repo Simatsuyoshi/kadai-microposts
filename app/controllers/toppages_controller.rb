a0a9f24ebc18e9f112588900cbf03c3a3e67d83f
tsuyoshi.shima <acc46trera@gmail.com>
2022-02-15 07:29:18 +0000

    user signup

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,5 @@
 class ToppagesController < ApplicationController
   def index
+    #render :index
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..60).each do |number|
+  User.create(name: 'name' + number.to_s, email: 'email@' + number.to_s, password: 'kjsa' + number.to_s)
+end
